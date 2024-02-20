e28748ff94b280664b3e0e9a3b57f66e7e4f9633
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2024-02-20 18:48:28 -0300

    att

--- streaming/lib/model/backend.dart
+++
@@ -1,7 +0,0 @@
-import 'mysql_db_configuration.dart';
-
-void main() async {
-  var connection = await MySqlDBconfiguration().connection;
-  var result = await connection!.query('select * from usuarios;');
-  print(result);
-}
--- streaming/lib/view/loginPage.dart
+++ streaming/lib/view/loginPage.dart
@@ -1,44 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'liveLy.dart';
-
-const users = {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-};
+import '../model/db_configuration.dart';
+import '../model/mysql_db_configuration.dart';
+import 'package:mysql1/mysql1.dart';
 
 class LoginPage extends StatelessWidget {
+  final DBConfiguration dbConfiguration = MySqlDBconfiguration();
+
   @override
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
+  Future<String?> _authUser(LoginData data) async {
+    try {
+      final MySqlConnection connection = await dbConfiguration.connection!;
+      final results = await connection.query(
+        "SELECT * FROM usuarios WHERE email = '${data.name}' AND senha = '${data.password}'",
+      );
+
+      if (results.isNotEmpty) {
+        return null;
+      } else {
+        return 'Credenciais inválidas';
       }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
+    } catch (e) {
+      print('Erro durante a autenticação: $e');
+      return 'Erro durante a autenticação';
+    }
   }
 
-  Future<String?> _signupUser(SignupData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
-    });
+  Future<String?> _signupUser(SignupData data) async {
+    try {
+      final MySqlConnection connection = await dbConfiguration.connection!;
+      final results = await connection.query(
+        "INSERT INTO usuarios (email, senha) VALUES ('${data.name}','${data.password}')",
+      );
+
+      if (results.affectedRows! > 0) {
+        return null;
+      } else {
+        return 'Erro ao cadastrar o usuário';
+      }
+    } catch (e) {
+      print('Erro durante o cadastro: $e');
+      return 'Erro durante o cadastro';
+    }
   }
 
-  Future<String?> _recoverPassword(String name) {
-    debugPrint('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
+  Future<String?> _recoverPassword(String name) async {
+    try {
+      final MySqlConnection connection = await dbConfiguration.connection!;
+      final results = await connection.query(
+        "SELECT * FROM usuarios WHERE email = '${name}'",
+      );
+
+      if (results.isNotEmpty) {
+        return 'Recuperação de senha enviada para o e-mail associado à conta.';
+      } else {
+        return 'Usuário não encontrado';
       }
-      return null;
-    });
+    } catch (e) {
+      print('Erro durante a recuperação de senha: $e');
+      return 'Erro durante a recuperação de senha';
+    }
   }
 
   @override
